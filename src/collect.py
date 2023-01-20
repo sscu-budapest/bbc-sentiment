481da997ca7e8af61b783648a9f1996916894d5c
Endre Márk Borza <endremborza@gmail.com>
2023-01-20 15:43:01 +0100

    add collection

---
+++ src/collect.py
@@ -0,0 +1,44 @@
+from itertools import product
+
+import datazimmer as dz
+import pandas as pd
+from aswan import RequestJsonHandler, add_url_params
+
+PAGES_TO_PULL = 4
+
+
+class BBCPatientCollector(RequestJsonHandler):
+
+    max_in_parallel = 4
+
+    def get_sleep_time(self):
+        return 2.5
+
+
+class Collect(dz.DzAswan):
+    name = "bbc-local"
+    cron = "15 9,11,16 * * *"
+
+    def prepare_run(self):
+
+        push_ids = (
+            pd.read_table("locales.md", sep="|", header=0, skipinitialspace=True)
+            .dropna(axis=1, how="all")
+            .iloc[1:, -1]
+            .dropna()
+        )
+        argprod = product(list(range(1, PAGES_TO_PULL + 1)), push_ids)
+        all_push_urls = list(map(create_push_url, argprod))
+        self.starters = {BBCPatientCollector: all_push_urls}
+
+
+def create_push_url(args):
+    pnum, pager_id = args
+    is_uk = "false"
+    limit = 20
+    push_base = "https://push.api.bbci.co.uk/batch"
+    push_t = (
+        f"/data/bbc-morph-lx-commentary-data-paged/assetUri/news%2Flive%2F{pager_id}"
+        f"/isUk/{is_uk}/limit/{limit}/nitroKey/lx-nitro/pageNumber/{pnum}/version/1.5.6?timeout=5"
+    )
+    return add_url_params(push_base, {"t": push_t})
